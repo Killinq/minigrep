43c0d6f03db6cacdf9a66c983487b6cdf9d2af03
Killinq <andrewalbert1808@gmail.com>
2022-05-11 16:34:07 -0400

    it is done amen :pray:

---
+++ target/debug/deps/minigrep-bc561cbc82eecdb1.d
@@ -0,0 +1,5 @@
+C:\Users\andre\Desktop\rust\minigrep\target\debug\deps\minigrep-bc561cbc82eecdb1.exe: src\lib.rs
+
+C:\Users\andre\Desktop\rust\minigrep\target\debug\deps\minigrep-bc561cbc82eecdb1.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/minigrep-f6b7bcbab2a2e987.d
@@ -0,0 +1,5 @@
+C:\Users\andre\Desktop\rust\minigrep\target\debug\deps\minigrep-f6b7bcbab2a2e987.exe: src\main.rs
+
+C:\Users\andre\Desktop\rust\minigrep\target\debug\deps\minigrep-f6b7bcbab2a2e987.d: src\main.rs
+
+src\main.rs:
